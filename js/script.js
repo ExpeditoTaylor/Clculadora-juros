d5dd5011885ce3c327edb42ae5cd08e7bf2839e8
Francisco Expedito <144634627+ExpeditoTaylor@users.noreply.github.com>
2023-09-11 10:28:57 -0300

    Calculadora de juros. Primeiro commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+document.getElementById('calcular').
+addEventListener('click', function(){
+    const value = document.getElementById('value').
+    value;
+
+    const free = document.getElementById('free').value;
+
+    const time = document.getElementById('time').value;
+
+    const total = value * (1 + free) ** time;
+
+    document.getElementById('total').innerHTML = ("R$ " + total.toFixed(2).replace('.', ','));
+    
+})
